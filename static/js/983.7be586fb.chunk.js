405b7bea89d205a5639f4e22b60304d31bb54a82
dyonns <117724779+dyonns@users.noreply.github.com>
2023-04-09 16:19:58 +0000

    Deploying to gh-pages from @ dyonns/goit-react-hw-05-movies@4e54bf8e71fa324969728ced78dccfc3f4104c59 🚀

---
+++ static/js/983.7be586fb.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[983],{983:function(e,r,t){t.r(r);var n=t(87),i=t(184);r.default=function(e){var r=e.movies;return(0,i.jsxs)(i.Fragment,{children:[(0,i.jsx)("h2",{children:"PopularFilms"}),(0,i.jsx)("ul",{children:r.map((function(e){return(0,i.jsx)("li",{children:(0,i.jsx)(n.rU,{to:"/movies/".concat(e.id),children:e.title})},e.id)}))})]})}}}]);
+//# sourceMappingURL=983.7be586fb.chunk.js.map
